c52e6c56769f121846e6dc1f47b3de7bee646972
JuanEspinoH <108641768+JuanEspinoH@users.noreply.github.com>
2025-04-03 18:51:16 +0000

    fisrt

---
+++ src/js/components/TodoList/TodoList.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from "react";
+import "./TodoListStyles.css";
+
+const TodoContainer = ({ todoText, index, setList }) => {
+  const [showButton,setShowButton]=useState(false)
+
+  const deleteHandler = () => {
+    setList((prev) => prev.filter((e) => e.id !== index));
+  };
+
+  const showButtonHandler=()=>setShowButton((prev)=>!prev)
+  return (
+    <div className="todoContainer " onMouseEnter={showButtonHandler} onMouseLeave={showButtonHandler}>
+      <p className="m-0">{todoText}</p>
+      {
+        showButton &&   <button onClick={deleteHandler} className="btn btn-danger ">
+        X
+      </button>
+      }
+    
+    </div>
+  );
+};
+
+const TodoList = () => {
+  const [list, setList] = useState([]);
+  const [text, setText] = useState({ id: 0, text: "" });
+
+  const changeHandler = (e) => {
+    setText({ id: list.length + 1, text: e.target.value });
+  };
+
+
+
+  const addTaskHandler=(e)=>{
+    if(e.key==="Enter"){
+     
+      setList((prev) => [...prev, text]);
+      setText({ id: 0, text: "" });
+    }
+  }
+  return (
+    <div className="mainContainer">
+      <input
+        type="text"
+        onKeyDown={addTaskHandler}
+        onChange={changeHandler}
+        value={text.text}
+        placeholder="Escribe aqui."
+        className="p-2"
+      />
+
+      {
+      list.length !== 0 ? 
+        
+        <div className="listContainer ">
+          {list.map((elemnt, i) => (
+            <TodoContainer
+              key={i}
+              todoText={elemnt.text}
+              index={elemnt.id}
+              setList={setList}
+            />
+          ))}
+        </div>
+      :
+      <p className="m-0">No tasks, add a task</p>
+      
+      }
+      <div className="align-self-start p-2 ">
+        {`${list.length} ${list.length === 1 ? "item" : "items"} `}
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
